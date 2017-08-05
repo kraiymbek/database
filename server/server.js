ee4e8bf0d38023e000f366e1edd023019a6e61a1
kraiymbek <kraiymbek@yahoo.com>
2017-08-05 16:24:10 +0600

    added GET request

--- server/server.js
+++ server/server.js
@@ -17,12 +17,23 @@ app.post('/todos',(req,res)=>{
 
     todo.save().then((docs)=>{
         res.send(docs)
-    },(e)=>{});
+    },(e)=>{
+        res.status(400).send(e);
+    });
 });
+
+app.get('/todos',(req,res)=>{
+    Todo.find().then(todos=>{
+        res.send({todos});
+    },err => res.status(400).send(e));
+});
+
 
 app.listen(3000,()=>{
     console.log('Startde on port 3000');
 });
+
+module.exports = {app};
 
 
 
